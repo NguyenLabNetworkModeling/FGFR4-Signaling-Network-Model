5b142266ad3e04f4e5e7a60d2818123315d8c46d
NguyenLabNetworkModeling <89375868+NguyenLabNetworkModeling@users.noreply.github.com>
2024-05-10 09:35:09 +1000

    Delete shared codes directory

--- shared codes/data_normalization.m
+++
@@ -1,9 +0,0 @@
-function dat = data_normalization(dat,dim)
-% (1) row-wise
-% (2) column-wise
-
-if dim == 2
-    dat = dat./repmat(dat(1,:),size(dat,1),1);
-elseif dim == 1
-    dat = dat./repmat(dat(:,1),1,size(dat,2));
-end
